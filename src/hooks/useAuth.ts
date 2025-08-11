488455649c4d83b8102ed89cfbdf5c71ec1209ff
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-08-11 22:54:13 +0530

    chore(supabase): configure and export Supabase client instance

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,31 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { supabase } from '@/lib/supabaseClient';
+import { User } from '@supabase/supabase-js';
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_eveent, session) => {
+      setUser(session?.user ?? null);
+      setLoading(false);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+
+  }, []);
+  
+  return {user, loading}
+}
