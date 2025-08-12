afae26fd3d702b70e88cd6f2cfc412bd2bbd2710
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-08-12 17:33:21 +0530

    Add Signup Page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,29 +1,59 @@
+'use client'
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const router = useRouter()
+
+  async function handleSignUp(e: React.FormEvent){
+    e.preventDefault();
+    setLoading(true)
+    setError(null)
+
+    const { error } = await supabase.auth.signUp({email, password});
+
+    setLoading(false)
+    if (error){
+      setError(error.message)
+    } else{
+      router.push('/signin')
+    }
+  }
+
   return (
     <>
       <div className="flex items-center justify-center min-h-screen">
         <Card className="w-full max-w-sm">
           <CardHeader>
             <CardTitle className="text-2xl">Sign Up</CardTitle>
-            <CardDescription>Enter your email below to create your account</CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
+            <form onSubmit={handleSignUp}>
               <div className="grid w-full items-center gap-4 ">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="name@example.com" />
+                  <Input id="email" type="email" placeholder="name@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="password">Password</Label>
-                  <Input id="password" type="password" placeholder="Your Password" />
+                  <Input id="password" type="password" placeholder="Your Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                  />
                 </div>
-                <Button className="w-full">Create Account</Button>
+                <Button className="w-full" >Create Account</Button>
               </div>
             </form>
           </CardContent>
