ffcfc0cc14bde5cacecf0735cafd7e1163d87ee5
Siddhartha Sharma <siddhartha1234sharma@gmail.com>
2025-08-13 07:46:21 +0530

    Add Dashboard Page

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,11 +1,16 @@
+import { Navbar } from "@/components/Navbar";
 import { SignOutButton } from "@/components/SignOutButton";
 
 export default function DashboardPage(){
   return(
+    <div>
+      <div>
+        <Navbar />
+      </div>
     <div className="flex min-h-screen justify-center items-center flex-col">
       <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
       <p className="text-2xl mb-6">You are logged in! 🎉</p>
-      <SignOutButton />
+    </div>
     </div>
   )
 }
